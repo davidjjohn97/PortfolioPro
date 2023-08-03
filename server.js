80a5c4641c34204521b65950d8e0e0a2889eaa3d
David Johnson <davidjjohn97@gmail.com>
2023-08-02 20:04:24 -0400

    make it better

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ if(process.env.NODE_ENV === "production"){
     );
 }
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 app.listen(port, console.log(`Server listening to ${port}`));
